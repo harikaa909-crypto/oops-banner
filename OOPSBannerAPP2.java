7428b5f1c14d8ca8e311e504e411b9f888f5a118
harikaa909-crypto <harikaa909@gmail.com>
2026-02-18 21:36:12 +0530

    Add files via upload

---
+++ OOPSBannerAPP2.java
@@ -0,0 +1,13 @@
+public class OOPSBannerAPP2 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "   *   ", "  ***  "));
+        System.out.println(String.join("", "  * *  ", " *   * "));
+        System.out.println(String.join("", " *   * ", " *   * "));
+        System.out.println(String.join("", " **", " ***"));
+        System.out.println(String.join("", " *   * ", " *   * "));
+        System.out.println(String.join("", " *   * ", " *   * "));
+        System.out.println(String.join("", " *   * ", " *   * "));
+    }
+
+}
